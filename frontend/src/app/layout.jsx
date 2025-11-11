e347c348803333c540ba86ca6bc2f405a46f0d62
THIAGO MENEZES TEIXEIRA <25170233@educ123.sp.senai.br>
2025-11-11 08:28:11 -0300

    Layout

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -23,11 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar/>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        <Navbar />
 
         {children}
 
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );

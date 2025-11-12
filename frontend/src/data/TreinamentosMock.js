be60a4566c051151c25fbb04b8f4baac00863a36
MIRELLA RAMOS DE OLIVEIRA <25170166@educ123.sp.senai.br>
2025-11-12 11:22:20 -0300

    Dados

---
+++ frontend/src/data/TreinamentosMock.js
@@ -0,0 +1,72 @@
+export const resumo = [
+  { titulo: 'Em Andamento', valor: 2 },
+  { titulo: 'Concluídos', valor: 3 },
+  { titulo: 'Agendados', valor: 2 },
+  { titulo: 'Taxa de Conclusão', valor: '87%' },
+];
+
+export const emAndamento = [
+  {
+    id: 1,
+    nome: 'Metodologias Ágeis na Prática',
+    tag: '65% concluído',
+    progresso: 65,
+    moduloAtual: 'Método Kanban',
+    modulos: '4/6',
+    horas: '7.8h/12h',
+    prazo: '30/11/2025',
+  },
+];
+
+export const concluidos = [
+  {
+    id: 1,
+    nome: 'Segurança da Informação',
+    area: 'Tecnologia',
+    data: '20/10/2025',
+    duracao: '10h',
+    nota: '92%',
+    certificado: '',
+  },
+  {
+    id: 2,
+    nome: 'Excel Avançado',
+    area: 'Análise de Dados',
+    data: '05/10/2025',
+    duracao: '8h',
+    nota: '88%',
+    certificado: '',
+  },
+  {
+    id: 3,
+    nome: 'Comunicação Corporativa',
+    area: 'Soft Skills',
+    data: '15/09/2025',
+    duracao: '6h',
+    nota: '95%',
+    certificado: '',
+  },
+];
+
+export const agendados = [
+  {
+    id: 1,
+    nome: 'Inovação e Design Thinking',
+    data: '10/12/2025',
+    tag: 'Inovação',
+    modalidade: 'Online',
+    duracao: '8h',
+    local: 'Teams',
+  },
+  {
+    id: 2,
+    nome: 'Análise de Dados com Power BI',
+    data: '05/12/2025',
+    tag: 'Análise de Dados',
+    modalidade: 'Online',
+    duracao: '16h',
+    local: 'Teams',
+  },
+];
+
+  

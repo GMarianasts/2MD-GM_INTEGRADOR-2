7c9b92f958a9d9be4d123d70ddd22cf99601c8e0
MARIA EDUARDA DE OLIVEIRA <25170160@educ123.sp.senai.br>
2025-11-14 11:01:15 -0300

    Histórico Admin

---
+++ frontend/src/app/historicoAdmin/page.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./historicoAdmin.css";
+import Link from "next/link";
+
+export default function DashboardHistorico() {
+    return (
+        <div className="container-fluid pagina-dashboardInscricoes">
+            <div className="row g-0">
+
+                <aside className="col-12 col-md-3 col-lg-2 bg-white border-end p-3 sidebar">
+                    <ul className="list-unstyled menu">
+                        <li className="ativo mb-3 d-flex align-items-center gap-2">
+                            <i className="bi bi-house-door"></i>
+                            <span>Dashboard</span>
+                        </li>
+                        <li className="mb-3 d-flex align-items-center gap-2">
+                            <i className="bi bi-grid"></i>
+                            <span>Gerenciar Treinamentos</span>
+                        </li>
+                        <li className="mb-3 d-flex align-items-center gap-2">
+                            <i className="bi bi-bar-chart"></i>
+                            <span>Relatório de Skill Gap</span>
+                        </li>
+                        <li className="d-flex align-items-center gap-2">
+                            <i className="bi bi-person"></i>
+                            <span>Gerencial Colaboradores</span>
+                        </li>
+                    </ul>
+                </aside>
+
+                <main className="col-12 col-md-9 px-4 py-4">
+                    <section className="introducao mb-4">
+                        <div className="trocaPagina">
+                            <ul className="nav nav-pills nav-fill">
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" href={"/dashboardAdmin"}>Dashboard</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" href={"/inscricoesAdmin"}>Inscrições</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" href={"/presencaAdmin"} aria-disabled="true">Presença</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" href={"#"} aria-disabled="true">Certificados</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link active" href={"/historicoAdmin"} aria-disabled="true">Histórico</Link>
+                                </li>
+                            </ul>            
+                        </div>
+                    </section>
+
+                    <div className="container-fluid my-2">
+                        <div className="row g-3">
+                            <div className="col-12">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+}

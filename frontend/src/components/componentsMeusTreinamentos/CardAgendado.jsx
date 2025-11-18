5e8351bea569085a230527e0c4e2861deba916e1
MIRELLA RAMOS DE OLIVEIRA <25170166@educ123.sp.senai.br>
2025-11-18 11:00:30 -0300

    Atualização components

--- frontend/src/components/componentsMeusTreinamentos/CardAgendado.jsx
+++ frontend/src/components/componentsMeusTreinamentos/CardAgendado.jsx
@@ -3,7 +3,6 @@ export default function CardAgendado({ t }) {
     <div className="card card-agendado border-0 shadow-sm rounded-4 p-4 mb-3">
       <div className="d-flex justify-content-between align-items-center mb-2">
         <div className="d-flex align-items-center gap-2">
-          <i className="bi bi-calendar-check text-warning fs-4"></i>
           <h5 className="fw-bold text-dark mb-0">{t.nome}</h5>
         </div>
         <span className="badge bg-light text-warning">{t.tag}</span>
--- frontend/src/components/componentsMeusTreinamentos/ResumoCard.jsx
+++ frontend/src/components/componentsMeusTreinamentos/ResumoCard.jsx
@@ -1,11 +1,23 @@
-export default function ResumoCard({ titulo, valor }) {
-    return (
-      <div className="col">
-        <div className="card shadow-sm border-0 rounded-4 text-center p-3 h-100">
-          <h6 className="text-secondary mb-2">{titulo}</h6>
-          <h3 className="fw-bold text-dark">{valor}</h3>
+import React from 'react';
+
+export default function ResumoCard({ titulo, valor, iconClass, iconBgColor }) {
+  return (
+    <div className="col">
+    
+      <div className="card shadow-sm border-0 rounded-4 p-3 h-100 resumo-card-layout">
+      
+        <div className="d-flex justify-content-between align-items-center">
+          
+          <div className="text-start">
+            <h6 className="text-secondary mb-0 resumo-card-titulo">{titulo}</h6> 
+            <h3 className="fw-bold text-dark mb-0 resumo-card-valor">{valor}</h3>
+          </div>
+
+          <div className="resumo-card-icon-container" style={{ backgroundColor: iconBgColor }}>
+            <i className={`bi ${iconClass} fs-5`}></i>
+          </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}

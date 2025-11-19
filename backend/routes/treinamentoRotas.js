7339d2d244a7e7f50aa68ddc45261bdce4384c23
MARIA EDUARDA DE OLIVEIRA <25170160@educ123.sp.senai.br>
2025-11-19 11:10:13 -0300

    Rota Treinamentos

--- backend/routes/treinamentoRotas.js
+++ backend/routes/treinamentoRotas.js
@@ -1,10 +1,8 @@
 import express from 'express';
-// ADICIONEI O criarTreinamento AQUI NA IMPORTAÇÃO V
 import { listarTreinamentos, criarTreinamento } from '../controllers/TreinamentoController.js';
 
 const router = express.Router();
 
-// Rota GET para http://localhost:3001/api/treinamentos
 router.get('/', listarTreinamentos);
 router.post('/', criarTreinamento);
 

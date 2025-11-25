1a8ac5c4b0b865f4a730cefc1b35102bbd64b428
MARIA EDUARDA DE OLIVEIRA <25170160@educ123.sp.senai.br>
2025-11-25 10:17:18 -0300

    Rotas Treinamento Atualizada

--- backend/routes/treinamentoRotas.js
+++ backend/routes/treinamentoRotas.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { listarTreinamentos, criarTreinamento, excluirTreinamento, atualizarTreinamento } from '../controllers/TreinamentoController.js';
+import { listarTreinamentos, criarTreinamento, excluirTreinamento, atualizarTreinamento, buscarTreinamentoPorId } from '../controllers/TreinamentoController.js';
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get('/', listarTreinamentos);
 router.post('/', criarTreinamento);
 router.delete('/:id', excluirTreinamento);
 router.put('/:id', atualizarTreinamento);
+router.get('/:id', buscarTreinamentoPorId);
 
 export default router;

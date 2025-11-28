b477d44a519e345e88124e116e7dcd55498ed3cc
Maria Eduarda <maria06k.study@gmail.com>
2025-11-28 01:03:38 -0300

    Atualizado

--- backend/routes/inscricaoRoutes.js
+++ backend/routes/inscricaoRoutes.js
@@ -6,6 +6,5 @@ const router = express.Router();
 router.get("/", InscricaoController.listar);
 router.post("/", InscricaoController.criar);
 router.delete("/:id", InscricaoController.excluir); // 👈 ou excluirInscricao (depende do nome)
-router.get("/usuario/:id", InscricaoController.listarPorUsuario);
 
 export default router;

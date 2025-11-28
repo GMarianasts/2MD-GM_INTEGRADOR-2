59d4c66400941adbadd897f43b880e7460c2d35e
MIRELLA RAMOS DE OLIVEIRA <25170166@educ123.sp.senai.br>
2025-11-28 10:34:06 -0300

    Atualização Dashboard

--- frontend/src/app/dashboardAdmin/page.jsx
+++ frontend/src/app/dashboardAdmin/page.jsx
@@ -67,7 +67,7 @@ const [totalColaboradores, setTotalColaboradores] = useState(0);
             <div className="row g-0">
 
                 {/* SIDEBAR RESPONSIVA */}
-               <aside className="d-none d-md-block col-md-3 col-lg-2 bg-white border-end p-3 sidebar">
+               <aside className="d-none d-md-block col-md-3 col-lg-2 bg-white border-end p-3 menu-lateral">
                     <ul className="list-unstyled menu">
                         <li className="ativo mb-3 d-flex align-items-center gap-2">
                             <i className="bi bi-house-door"></i>

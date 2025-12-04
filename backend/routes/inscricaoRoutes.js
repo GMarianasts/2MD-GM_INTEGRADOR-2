103dded72793299ed8eff2c3cf0597f17f1f5056
MARIA EDUARDA DE OLIVEIRA <25170160@educ123.sp.senai.br>
2025-12-04 08:08:47 -0300

    Atualização Rotas inscrição

--- backend/routes/inscricaoRoutes.js
+++ backend/routes/inscricaoRoutes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 // Rotas CRUD para inscrição
 router.get("/", InscricaoController.listar); // Listar todas as inscrições
+router.get("/usuario/:id", InscricaoController.listarPorUsuario);
 router.post("/", InscricaoController.criar); // Criar inscrição
-router.put("/concluir/:id", InscricaoController.concluir); // Concluir inscrição
+router.put("/:id/concluir", InscricaoController.concluir); // Concluir inscrição
 router.delete("/:id", InscricaoController.excluir); // Excluir inscrição
 
 export default router;
